4ecf75d4d70f3c1d7cc3750006605a2cfedde096
ibookhotel <info@ibookhotel.me>
2016-03-23 10:26:41 +0100

    Added files via upload

---
+++ curlLib/curlWrap_v1.php
@@ -0,0 +1,64 @@
+<?php
+
+    /**
+     * ibookhotel \ Curl Wrap
+     *
+     * The Curl Wrap is the entry point to all services and actions.
+     */
+
+
+    # Enter your domain name , user id and user api key
+    define("REST_DOMAIN", "localhost/jsonapi/public");  # Example : define("REST_DOMAIN","jsonapi/manage-booking.com");
+    define("ACCOUNT_ID", "YOUR_ACCOUNT_ID");
+    define("REST_API_KEY", "YOUR_REST_API_KEY");
+
+    function curlWrap($entity, $data, $method, $content_type) {
+        if ($content_type == NULL) {
+            $content_type = "application/json";
+        }
+
+        $api_url = "http://" . REST_DOMAIN . "/" . $entity;
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+        curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+        curl_setopt($ch, CURLOPT_UNRESTRICTED_AUTH, TRUE);
+        switch ($method) {
+            case "POST":
+                $url = $api_url;
+                curl_setopt($ch, CURLOPT_URL, $url);
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+                break;
+            case "GET":
+                $url = $api_url;
+                curl_setopt($ch, CURLOPT_URL, $url);
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+                break;
+            case "PUT":
+                $url = $api_url;
+                curl_setopt($ch, CURLOPT_URL, $url);
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+                break;
+            case "DELETE":
+                $url = $api_url;
+                curl_setopt($ch, CURLOPT_URL, $url);
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+                break;
+            default:
+                break;
+        }
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            "Content-type : $content_type;",
+            'Accept : application/json'
+        ));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_USERPWD, ACCOUNT_ID . ':' . REST_API_KEY);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 120);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        $output = curl_exec($ch);
+        curl_close($ch);
+
+        return $output;
+    }
